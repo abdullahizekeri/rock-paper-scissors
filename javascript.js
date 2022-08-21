af0fc2cb5528356ea8731d845037bd6e4b1e9b8c
abdullahizekeri <abdullahizekeri742@gmail.com>
2022-08-21 08:56:12 +0100

    plaRound between plaerSelection and computerSelection!

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+const myArray = ['Rock','Paper','Scissors'];
+function getComputerChoice(){
+    return myArray[~~(Math.random()*myArray.length)];
+}
+
+playerSelection = "rock";
+playerSelection = playerSelection.toLowerCase();
+computerSelection = getComputerChoice().toLowerCase();
+
+function playRound(playerSelection,computerSelection){
+if(playerSelection==computerSelection){
+    console.log("You Tie!");
+} else if((computerSelection=='rock' && playerSelection=='paper') 
+        || (computerSelection=='paper' && playerSelection=='scissors') 
+        || (computerSelection=='scissors' && playerSelection=='rock'))    
+    {
+    console.log('You loose! ' +computerSelection+ ' beats ' +playerSelection)
+    } 
+    else{
+        console.log('You Win! '+playerSelection+ ' beats ' +computerSelection)
+    }
+}
+
+
+console.log(playRound(playerSelection,computerSelection))
