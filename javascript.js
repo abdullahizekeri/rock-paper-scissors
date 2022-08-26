7c7cc0b9b3a14bc1100900eafff362c8400e67a6
abdullahizekeri <abdullahizekeri742@gmail.com>
2022-08-26 11:24:01 +0100

    Added button functions/decleration and css style

--- javascript.js
+++ javascript.js
@@ -2,32 +2,22 @@ const myArray = ['Rock','Paper','Scissors'];
 function getComputerChoice(){
     return myArray[~~(Math.random()*myArray.length)];
 }
+
 let computerSelection;
 let playerSelection;
 let playerScore;
 let computerScore;
 
-/*playerSelection = "rock"; #deleted*/
-
-function playRound(playerSelection,computerSelection){
-playerSelection = playerSelection.toLowerCase();
-computerSelection = getComputerChoice().toLowerCase();
-
-if(playerSelection==computerSelection){
-    alert("Tie Game!");
-    return 'Tie Game\nComputer Score: ' + computerScore + '\nYour Score: ' + playerScore;
-} 
-else if((computerSelection=='rock' && playerSelection=='paper') 
-        || (computerSelection=='paper' && playerSelection=='scissors') 
-        || (computerSelection=='scissors' && playerSelection=='rock'))    
-    {
-        alert('You loose! ' +computerSelection+ ' beats ' +playerSelection)
-        return 'Computer Score: ' + ++computerScore + '\nYour Score: ' + playerScore;
-    } 
-else{
-        console.log('You Win! '+playerSelection+ ' beats ' +computerSelection)
-    }
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+button.addEventListener("click",() => {
+playerSelection=button.innerText.toLowerCase();
+playRound(playerSelection,computerSelection);
+if(playerScore===5 && computerScore===5){
+    declareWinner();
 }
+});
+});
 
 function game(){
     computerScore = 0;
@@ -58,7 +48,28 @@ function onUserInput() {
           ||(playerSelection.toLowerCase()=='scissors')) {
         console.log(playRound(playerSelection,computerSelection));
       } else {
-      alert('That\'s not a valid choice.');
+      alert('Thats not a valid choice.');
       onUserInput();
     }
-  }
+}
+
+  function playRound(playerSelection,computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+    
+    if(playerSelection==computerSelection){
+        alert("Tie Game!");
+        return 'Tie Game\nComputer Score: ' + computerScore + '\nYour Score: ' + playerScore;
+    } 
+    else if((computerSelection=='rock' && playerSelection=='paper') 
+            || (computerSelection=='paper' && playerSelection=='scissors') 
+            || (computerSelection=='scissors' && playerSelection=='rock'))    
+        {
+            alert('You loose! ' +computerSelection+ ' beats ' +playerSelection)
+            return 'Computer Score: ' + ++computerScore + '\nYour Score: ' + playerScore;
+        } 
+    else{
+            console.log('You Win! '+playerSelection+ ' beats ' +computerSelection)
+        }
+    }
+    
